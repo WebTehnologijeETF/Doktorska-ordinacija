2028ad428c92509e43f8c3b4ece9c3fb8f288554
Emina Šarić <esaric1@etf.unsa.ba>
2015-04-15 21:50:37 +0200

    Popravljeno nekoliko stvari

---
+++ stablo.js
@@ -0,0 +1,37 @@
+document.getElementById("2015").addEventListener("click", pokaziMjesece);
+document.getElementById("2014").addEventListener("click", pokaziMjesece);
+
+function pokaziMjesece(event) {
+    var godina = event.currentTarget;
+    if (godina.childElementCount == 0) {
+        var mjeseci = ["Januar", "Februar", "Mart", "April", "Maj", "Juni",
+            "Juli", "August", "Septembar", "Oktobar", "Novembar", "Decembar"],
+			listaMjeseci = document.createElement("UL"), i;
+        for (i = 0; i < 12; i++) {
+            var mjesec = document.createElement("LI");
+            mjesec.appendChild(document.createTextNode(mjeseci[i]));
+            mjesec.className = i;
+            mjesec.addEventListener("click", pokaziDane);
+            listaMjeseci.appendChild(mjesec);
+        }
+        godina.appendChild(listaMjeseci);
+    } else {
+        //godina.removeChild(godina.firstElementChild);
+    }
+}
+
+function pokaziDane(event) {
+    var mjesec = event.currentTarget;
+    if (mjesec.childElementCount == 0) {
+        var dani = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        var listaDana = document.createElement("UL");
+        for (var i = 1; i <= dani[mjesec.className]; i++) {
+            var dan = document.createElement("LI");
+            dan.appendChild(document.createTextNode(i));
+            listaDana.appendChild(dan);
+        }
+        mjesec.appendChild(listaDana);
+    } else {
+        mjesec.removeChild(mjesec.firstElementChild);
+    }
+}
