c2fdbd18a81ece82a2e2df22d7d29bdb3ad733d2
Emina Šarić <esaric1@etf.unsa.ba>
2015-04-15 20:02:30 +0200

    Stablo sa dva nivoa

--- skripta.js
+++ skripta.js
@@ -1,20 +1,7 @@
-document.getElementById("mail").addEventListener("blur",validirajMail);
-document.getElementById("poruka").addEventListener("blur",validirajPoruku);
-
-function validiraj() {
-	var ime=document.getElementById("ime"), mail=document.getElementById("mail"),
-	subject=document.getElementById("subject"), poruka=document.getElementById("poruka"),
-	greska_mail=document.getElementById("greska_mail");
-	if(mail.value=="" || mail.value=="E-mail")
-	{
-		greska_mail.style.visibility="visible";
-		mail.style.backgroundColor="#FFCCCC";
-	}
-	if(poruka.value=="")
-	{
-		greska_poruka.style.visibility="visible";
-	}
-}
+document.getElementById("mail").addEventListener("blur", validirajMail);
+document.getElementById("poruka").addEventListener("blur", validirajPoruku);
+document.getElementById("2015").addEventListener("click", function() {stablo("2015");});
+document.getElementById("2014").addEventListener("click", function() {stablo("2014");});
 
 function validirajMail() {
 	var mail=document.getElementById("mail"), greska_mail=document.getElementById("greska_mail");
@@ -39,5 +26,23 @@ function validirajPoruku() {
 	}
 	else {
 		greska_poruka.style.visibility="hidden";
+	}
+}
+
+function stablo(node) {
+	var godina = document.getElementById(node);
+	if(godina.childElementCount == 0) {
+		var mjeseci=["Januar","Februar","Mart","April","Maj","Juni",
+		"Juli","August","Septembar","Oktobar","Novembar","Decembar"];
+		var listaMjeseci=document.createElement("UL");
+		for(var i=0;i<12;i++) {
+			var mjesec=document.createElement("LI");
+			mjesec.appendChild(document.createTextNode(mjeseci[i]));
+			listaMjeseci.appendChild(mjesec);
+		}
+		godina.appendChild(listaMjeseci);
+	}
+	else {
+		godina.removeChild(godina.firstElementChild);
 	}
 }
