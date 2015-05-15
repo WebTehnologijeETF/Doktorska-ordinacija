e23b93d2d9154ade1807208b690c9f6caecb0884
Emina Šarić <esaric1@etf.unsa.ba>
2015-05-15 18:20:44 +0200

    PHP - Validacija obaveznih polja

--- ajax.js
+++ ajax.js
@@ -1,25 +1,3 @@
-function validirajMail() {
-    var mail = document.getElementById("mail"),
-        greska_mail = document.getElementById("greska_mail"), regex = /.+\@.+\..+/;
-
-    if (!regex.test(mail.value)) {
-        greska_mail.style.visibility = "visible";
-        mail.style.backgroundColor = "#FFCCCC";
-    } else if (regex.test(mail.value)) {
-        greska_mail.style.visibility = "hidden";
-        mail.style.backgroundColor = "white";
-    }
-}
-
-function validirajPoruku() {
-    var poruka = document.getElementById("poruka");
-    if (poruka.value == "") {
-        greska_poruka.style.visibility = "visible";
-    } else {
-        greska_poruka.style.visibility = "hidden";
-    }
-}
-
 function otvori_stranicu(stranica) {
 	var ajax = new XMLHttpRequest();
 	ajax.onreadystatechange = function() {
