df7b3c955873b8d1d308405440ffca5de9cca3ec
Emina Šarić <esaric1@etf.unsa.ba>
2015-05-30 23:22:23 +0200

    Dump baze

---
+++ dump.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 30, 2015 at 11:20 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ordinacijasajt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentar`
+--
+
+CREATE TABLE IF NOT EXISTS `komentar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vijest` int(11) NOT NULL,
+  `tekst` text COLLATE utf8_slovenian_ci NOT NULL,
+  `autor` varchar(20) COLLATE utf8_slovenian_ci NOT NULL,
+  `vrijeme` timestamp NOT NULL,
+  `mail` varchar(50) COLLATE utf8_slovenian_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `vijest` (`vijest`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `komentar`
+--
+
+INSERT INTO `komentar` (`id`, `vijest`, `tekst`, `autor`, `vrijeme`, `mail`) VALUES
+(1, 1, 'Bravo!', 'Ja', '2015-05-14 10:28:56', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vijest`
+--
+
+CREATE TABLE IF NOT EXISTS `vijest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `naslov` varchar(100) COLLATE utf8_slovenian_ci NOT NULL,
+  `tekst` text COLLATE utf8_slovenian_ci NOT NULL,
+  `autor` varchar(20) COLLATE utf8_slovenian_ci NOT NULL,
+  `vrijeme` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `vijest`
+--
+
+INSERT INTO `vijest` (`id`, `naslov`, `tekst`, `autor`, `vrijeme`) VALUES
+(1, 'Dodan prvi rekord!', 'Prvi rekord u tabelu uspješno dodan', 'Emina', '2015-05-14 10:28:15');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`vijest`) REFERENCES `vijest` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
