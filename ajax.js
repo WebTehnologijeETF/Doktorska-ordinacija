f719a28ec03eaed000a4bd5651020a0b9f63e686
Emina Šarić <esaric1@etf.unsa.ba>
2015-06-04 11:23:27 +0200

    Minorne ispravke
    Greške uzrokovane preimenovanjem html fajlova u htm

--- ajax.js
+++ ajax.js
@@ -1,85 +1,85 @@
 function otvori_stranicu(stranica) {
-	var ajax = new XMLHttpRequest();
-	ajax.onreadystatechange = function() {
-		if (ajax.readyState == 4 && ajax.status == 200) {
-			document.getElementById("glavni").innerHTML = ajax.responseText;
-			klasa = stranica.slice(0, stranica.length - 4);
-			document.getElementById("glavni").className = klasa;
-			if(stranica == "kontakt.html") {
-				// document.getElementById("mail").addEventListener("blur", validirajMail);
-				// document.getElementById("poruka").addEventListener("blur", validirajPoruku);
-				document.getElementById("tel").addEventListener("blur", validirajTelefon);
-				popuniDrzave();
-			}
-		}
-		if (ajax.readyState == 4 && ajax.status == 404)
-			document.getElementById("glavni").innerHTML = "Greska: nepoznat URL";
-	}
-	ajax.open("GET", stranica, true);
-	ajax.send();
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            document.getElementById("glavni").innerHTML = ajax.responseText;
+            klasa = stranica.slice(0, stranica.length - 4);
+            document.getElementById("glavni").className = klasa;
+            if (stranica == "kontakt.htm") {
+                // document.getElementById("mail").addEventListener("blur", validirajMail);
+                // document.getElementById("poruka").addEventListener("blur", validirajPoruku);
+                document.getElementById("tel").addEventListener("blur", validirajTelefon);
+                popuniDrzave();
+            }
+        }
+        if (ajax.readyState == 4 && ajax.status == 404)
+            document.getElementById("glavni").innerHTML = "Greska: nepoznat URL";
+    }
+    ajax.open("GET", stranica, true);
+    ajax.send();
 }
 
 function popuniDrzave() {
-	var drzave = document.getElementById("drzave");
-	var ajax = new XMLHttpRequest();
-	ajax.onreadystatechange = function() {
-		if (ajax.readyState == 4 && ajax.status == 200) {
-			var spisakDrzava = JSON.parse(ajax.responseText);
-			for(var i = 0; i < spisakDrzava.length; i++) {
-				var opcija = document.createElement("OPTION");
-				var t = document.createTextNode(spisakDrzava[i].name);
-				opcija.appendChild(t);
-				drzave.appendChild(opcija);
-			}
-		}			
-		if (ajax.readyState == 4 && ajax.status == 404)
-			document.getElementById("greska_drzave").style.visibility = "visible";
-		if (ajax.readyState == 4 && ajax.responseText == "")
-			document.getElementById("greska_drzave").style.visibility = "visible";
-	}
-	ajax.open("GET", "https://restcountries.eu/rest/v1/all", true);
-	ajax.send();
+    var drzave = document.getElementById("drzave");
+    var ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+            var spisakDrzava = JSON.parse(ajax.responseText);
+            for (var i = 0; i < spisakDrzava.length; i++) {
+                var opcija = document.createElement("OPTION");
+                var t = document.createTextNode(spisakDrzava[i].name);
+                opcija.appendChild(t);
+                drzave.appendChild(opcija);
+            }
+        }
+        if (ajax.readyState == 4 && ajax.status == 404)
+            document.getElementById("greska_drzave").style.visibility = "visible";
+        if (ajax.readyState == 4 && ajax.responseText == "")
+            document.getElementById("greska_drzave").style.visibility = "visible";
+    }
+    ajax.open("GET", "https://restcountries.eu/rest/v1/all", true);
+    ajax.send();
 }
 
 function validirajTelefon() {
-	var tel = document.getElementById("tel");
-	var greska_tel = document.getElementById("greska_tel");
-	if(tel.value != "") {
-		var ajax = new XMLHttpRequest();
-		ajax.onreadystatechange = function() {
-			if (ajax.readyState == 4 && ajax.status == 200) {
-					var drzava = JSON.parse(ajax.responseText);
-					var pozivniBrojevi = drzava[0].callingCodes;					
-					for(var i = 0; i < pozivniBrojevi.length; i++) {
-						if(tel.value.search(pozivniBrojevi[i]) == 0) {
-							greska_tel.style.visibility = "hidden";
-							tel.style.backgroundColor = "white";
-							break;
-						}
-						else {
-							tel.style.backgroundColor = "#FFCCCC";
-							greska_tel.style.visibility = "visible";						
-						}
-					}
-				}
-			}
-			
-			if (ajax.readyState == 4 && ajax.status == 404) {
-				tel.style.backgroundColor = "#FFCCCC";
-				greska_tel.style.visibility = "visible";
-			}
-				
-			if (ajax.readyState == 4 && ajax.responseText == "") {
-				tel.style.backgroundColor = "#FFCCCC";
-				greska_tel.style.visibility = "visible";
-			}
-		
-		ajax.open("GET", "https://restcountries.eu/rest/v1/name/" + document.getElementById("drzave").value, true);
-		ajax.send();
-	}
-	
-	else {
-		greska_tel.style.visibility = "hidden";
-		tel.style.backgroundColor = "white";
-	}
+    var tel = document.getElementById("tel");
+    var greska_tel = document.getElementById("greska_tel");
+    if (tel.value != "") {
+        var ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                var drzava = JSON.parse(ajax.responseText);
+                var pozivniBrojevi = drzava[0].callingCodes;
+                for (var i = 0; i < pozivniBrojevi.length; i++) {
+                    if (tel.value.search(pozivniBrojevi[i]) == 0) {
+                        greska_tel.style.visibility = "hidden";
+                        tel.style.backgroundColor = "white";
+                        break;
+                    }
+                    else {
+                        tel.style.backgroundColor = "#FFCCCC";
+                        greska_tel.style.visibility = "visible";
+                    }
+                }
+            }
+        }
+
+        if (ajax.readyState == 4 && ajax.status == 404) {
+            tel.style.backgroundColor = "#FFCCCC";
+            greska_tel.style.visibility = "visible";
+        }
+
+        if (ajax.readyState == 4 && ajax.responseText == "") {
+            tel.style.backgroundColor = "#FFCCCC";
+            greska_tel.style.visibility = "visible";
+        }
+
+        ajax.open("GET", "https://restcountries.eu/rest/v1/name/" + document.getElementById("drzave").value, true);
+        ajax.send();
+    }
+
+    else {
+        greska_tel.style.visibility = "hidden";
+        tel.style.backgroundColor = "white";
+    }
 }
--- novosti.php
+++ novosti.php
@@ -1,5 +1,7 @@
 <?php
-$veza = new PDO("mysql:dbname=ordinacijaosmijeh;host=127.5.233.2;charset=utf8", "doktor", "doktorpass");
+$username = "doktor";
+$password = "doktorpass";
+$veza = new PDO("mysql:dbname=ordinacijaosmijeh;host=127.5.233.2;charset=utf8", $username, $password);
 $veza->exec("set names utf8");
 $rezultat = $veza->query("select v.id, v.naslov, v.tekst,
                         UNIX_TIMESTAMP(v.vrijeme) vrijeme2, v.autor, count(k.vijest) brKom 
@@ -26,29 +28,28 @@ foreach ($rezultat as $vijest) {
     else print '<small><a href="javascript:void(0)" onclick='
         . '"prikaziKomentare('.$vijest['id'].')">'.$vijest['brKom']." komentara</a></small><br>";
 
-    print '<span id="v'.$vijest['id'].'"></span>';
+    print '<span class="komentari" id="v'.$vijest['id'].'"></span>';
     print '<div id="form'.$vijest['id'].'" class="formaZaSlanjeKomentara">';
     print '
-        <form method="GET" action="index.php">
+        <form action="" method="GET">
         Vaše ime:<br>
-        <input type="text" name="autor" id="autor"><br>
+        <input type="text" name="autor" id="autor" required>*<br>
         Vaš mail:<br>
         <input type="text" name="mail" id="mail"><br>
-        <textarea rows="10" cols="30" name="komentar" id="komentar"></textarea><br>
+        <textarea rows="10" cols="30" name="komentar" id="komentar" required></textarea>*<br>
         <input type="submit" name="buttonPosalji" value="Pošalji komentar">
         <input type="hidden" name="vijest" id="vijest" value = "'.$vijest['id'].'">
         </form>';
-    print '<script type="text/javascript" src="posaljiKomentar.js"></script>';
     print '</div>';
 }
 
-if(isset($_REQUEST['vijest'])) {
+if(isset($_GET['vijest'])) {
     $filtriranaVijest = filter_input(INPUT_GET, 'vijest', FILTER_SANITIZE_NUMBER_INT);
     $filriraniKomentar = filter_input(INPUT_GET, 'komentar', FILTER_SANITIZE_STRING);
     $filtriraniAutor = filter_input(INPUT_GET, 'autor', FILTER_SANITIZE_STRING);
     $filtriraniMail = filter_input(INPUT_GET, 'mail', FILTER_SANITIZE_EMAIL);
 
-    $veza = new PDO("mysql:dbname=ordinacijaosmijeh;host=127.5.233.2;charset=utf8", "doktor", "doktorpass");
+    $veza = new PDO("mysql:dbname=ordinacijaosmijeh;host=127.5.233.2;charset=utf8", $username, $password);
     $veza->exec("set names utf8");
     $rezultat = $veza->query("INSERT INTO komentar SET vijest='".$filtriranaVijest."',
     tekst='".$filriraniKomentar."', mail='".$filtriraniMail."', autor='".$filtriraniAutor."'");
